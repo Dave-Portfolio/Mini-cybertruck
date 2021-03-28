23523ad56590e59ab22fcdea6a887cc2730cb4f1
agp5252 <81523016+agp5252@users.noreply.github.com>
2021-03-28 10:33:01 -0500

    Pwm values changed
    Changed Pwm values to 1 (Max) to solve motor driver issues

---
+++ Testcode.py
@@ -0,0 +1,50 @@
+import pygame, sys, time    #Imports Modules
+from pygame.locals import *
+from gpiozero import Robot, PWMLED
+
+
+
+pygame.init()#Initializes Pygame
+pygame.joystick.init()
+joystick = pygame.joystick.Joystick(0)
+joystick.init()
+
+#Initializes Joystick
+
+robot = Robot(left=("BOARD18","BOARD16"), right=("BOARD11","BOARD13")) #initializes robot
+
+axis0 = joystick.get_axis(0)
+axis1 = joystick.get_axis(1)
+               
+rightpwm = PWMLED("BOARD12")
+leftpwm = PWMLED("BOARD33")
+     
+
+
+while True:
+    pygame.event.get()
+    axis0 = joystick.get_axis(0)
+    axis1 = joystick.get_axis(1)
+    axis2 = joystick.get_axis(2)
+    axis3 = joystick.get_axis(3)
+    if (0.65<axis0):
+        robot.backward()
+        rightpwm.value = 1
+        leftpwm.value = 1
+    elif(-0.65>axis0):
+        robot.forward()
+        rightpwm.value = 1
+        leftpwm.value = 1
+    elif (0.65<axis1):
+        robot.right()
+        rightpwm.value = 1
+        leftpwm.value = 1
+    elif(-0.65>axis1):
+        robot.left()
+        rightpwm.value = 1
+        leftpwm.value = 1
+    else:
+        robot.stop()
+        rightpwm.value = 0
+        leftpwm.value = 0
+    print(axis1)
